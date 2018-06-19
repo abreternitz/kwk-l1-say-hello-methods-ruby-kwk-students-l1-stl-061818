62a24d0234e7b6b5047f5582457bc2e05befa57f
abreternitz <aliceburian@gmail.com>
2018-06-19 14:46:20 +0000

    automatically backed up by learn

--- morningroutine.rb
+++ morningroutine.rb
@@ -1,5 +1,6 @@
 def morning_routine
-  puts "Alarm goes off"
+  puts "Alarm goes off and i press snooze"
+  puts "alarm goes off and i press snooze"
   puts "I get out of bed"
   puts "change clothes"
 end
