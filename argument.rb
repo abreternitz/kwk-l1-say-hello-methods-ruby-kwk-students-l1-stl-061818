a24d95b9fd9afe53809ccf7e53ef526d43de0cb6
abreternitz <aliceburian@gmail.com>
2018-06-19 15:03:02 +0000

    automatically backed up by learn

--- argument.rb
+++ argument.rb
@@ -1,8 +1,8 @@
 
 
-def greeting (local_greeting,your_name,my_name,time_of_day)
-  local_greeting-gets.chomp.capitalize
+def greeting (your_name,my_name,time_of_day)
+  local_greeting-gets
   puts "#{local_greeting}, #{your_name}. I'm #{my_name}, how's your #{time_of_day}?"
 end
 
-greeting("howdy","jessica","alice","morning")
+greeting("jessica","alice","morning")
