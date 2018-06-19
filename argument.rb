54f70819b09ca7b90d78eccddccccc7ded481282
abreternitz <aliceburian@gmail.com>
2018-06-19 15:03:15 +0000

    automatically backed up by learn

--- argument.rb
+++ argument.rb
@@ -1,7 +1,7 @@
 
 
 def greeting (your_name,my_name,time_of_day)
-  local_greeting-gets
+  local_greeting=gets
   puts "#{local_greeting}, #{your_name}. I'm #{my_name}, how's your #{time_of_day}?"
 end
 
